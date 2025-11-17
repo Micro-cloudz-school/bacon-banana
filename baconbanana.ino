9f820b40ac65e3a46017967b9ced1d189c09a288
Micro-cloudz <kingl0506@wrdsb.ca>
2025-11-17 12:56:38 -0500

    Create baconbanana.ino

---
+++ baconbanana.ino
@@ -0,0 +1,38 @@
+const int redPin = 11;
+const int greenPin = 10;
+const int bluePin = 9;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+     String input = "";
+      if (Serial.available()) {
+    input = Serial.readStringUntil('\n'); // Read until newline
+    input.trim(); // Remove any spaces or newline characters
+    input.toLowerCase();
+      }
+
+if (input == "red") {
+       analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+    }
+
+  if (input == "green") {
+       analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);
+    }
+
+  if (input == "blue") {
+       analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 255);
+    }
+  
+}
